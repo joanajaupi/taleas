75c9da338aa16b091a5d862923ed9b82423b7979
unknown <joanajaupi01@gmail.com>
2023-07-14 16:38:29 +0200

    remove unnecessary files

--- task2/models/productModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-let ProductSchema = new mongoose.Schema(
-    {
-        productName: {type: String, reuqired: true},
-        productPrice: {type: Number, required: true},
-        categoryId: {type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true} //points to a single document in the Category collection
-    }
-)
-module.exports = mongoose.model("Product", ProductSchema);
